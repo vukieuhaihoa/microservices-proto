0830a1e4ab434341433cef4212010e2e2cdb0b7a
Millo Milk <millomilk2022@gmail.com>
2023-09-16 22:05:04 +0700

    feat: add tag script

--- scripts/buf-gen.sh
+++ scripts/buf-gen.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# This script to generate to Golang code from proto file use buf
 
 SERVICE_NAME=$1
 SERVICE_VERSION=$2
@@ -7,23 +9,41 @@ RELEASE_VERSION=$3
 # When a query returns a non-zero status, the -e flag stops the script.
 set -e
 
-# Helper func to go one path
-# Usage: gotoDir ./proto/shipping/v1
-function gotoDir() {
+#######################################
+# Go to path
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+# Example:
+# goto_dir ./gg/hh
+#######################################
+function goto_dir() {
    path=$1
    pushd $path >/dev/null
 }
 
-# Helper func to go to the previous path
-# Usage: leaveDir
-function leaveDir() {
+#######################################
+# Go to previous path
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+# Example:
+# goto_dir ./gg/hh
+#######################################
+function leave_dir() {
    popd >/dev/null
 }
 
-function initGoModAndTidy() {
+function init_go_mod_and_tidy() {
    # go to service folder
    echo "Go to folder ${SERVICE_NAME} and download dependencies"
-   gotoDir ./gen/go/${SERVICE_NAME}
+   goto_dir ./gen/go/${SERVICE_NAME}
 
    if [ -e go.mod ]; then
       echo "go.mod file is already!"
@@ -33,12 +53,12 @@ function initGoModAndTidy() {
    fi
    # download dependencies
    go mod tidy
-   echo "Install dependencies sucess!"
-   leaveDir
+   echo "Install dependencies success!"
+   leave_dir
 }
 
 # Generate code from *.proto file
-function bufGen() {
+function buf_gen() {
    echo "Lint and generate proto...."
    case $SERVICE_NAME in
    "order")
@@ -64,8 +84,6 @@ function bufGen() {
    echo "Lint and generate proto done!"
 }
 
-bufGen
+buf_gen
 
-initGoModAndTidy
-
-pwd
+init_go_mod_and_tidy
